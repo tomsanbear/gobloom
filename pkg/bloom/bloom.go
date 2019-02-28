3873d441c7a62c22719ea50dcf69f30b67275de1
Thomas Santerre <thomas@santerre.xyz>
2019-02-27 22:28:30 -0500

    More work done, finsihed bulk of the bloom package. Not showing accurate results at the moment, needs more investigation.

--- pkg/bloom/bloom.go
+++ pkg/bloom/bloom.go
@@ -3,33 +3,40 @@ package bloom
 import (
 	"fmt"
 	"hash"
+	"math"
+
+	"github.com/damnever/bitarray"
 
 	"github.com/spaolacci/murmur3"
 )
 
+// Bloom interface encapsulates our useful features
+type Bloom interface {
+	Add(item string) error
+	Check(item string) (bool, error)
+}
+
 type bloom struct {
 	// Public Vars
 	Capacity  int
-	ErrorRate float32
+	ErrorRate float64
 
 	// Private vars
-	hashers []hash.Hash64
+	numHashes    int
+	hashFuncs    []hash.Hash64
+	bitArraySize int
+	bitArray     *bitarray.BitArray
 }
 
 // New creates an instance of the bloom filter
-func New(hashicity, capacity int, errorRate float32) (bloom, error) {
+func New(capacity int, errorRate float64) (bloom, error) {
 	/*
 		Initialize the bloom filter here, need to do a few things
-		- Initialize the hash functions?
+		- Initialize the hash functions
 		- Create the hash array
 		- hopefully not die
 	*/
 	var b bloom
-
-	hashers := make([]hash.Hash64, 0)
-	for i := 0; i < hashicity; i++ {
-		hashers = append(hashers, murmur3.New64WithSeed(uint32(i)))
-	}
 
 	// Preconditions:
 	if capacity == 0 {
@@ -40,10 +47,79 @@ func New(hashicity, capacity int, errorRate float32) (bloom, error) {
 	}
 
 	// Initialize the hash functions
+	baSize := optimalBitArraySize(float64(capacity), errorRate)
+	nHashes := optimalHashFunk(baSize, capacity)
+
+	hashers := make([]hash.Hash64, 0)
+	for i := 0; i < nHashes; i++ {
+		hashers = append(hashers, murmur3.New64WithSeed(uint32(i)))
+	}
 
 	b = bloom{
-		Capacity:  capacity,
-		ErrorRate: errorRate,
+		Capacity:     capacity,
+		ErrorRate:    errorRate,
+		hashFuncs:    hashers,
+		bitArray:     bitarray.New(baSize),
+		bitArraySize: baSize,
+		numHashes:    nHashes,
 	}
+
+	fmt.Printf("created with array size: %v, using %v hash functions.\n", b.bitArraySize, b.numHashes)
+
 	return b, nil
+}
+
+func optimalBitArraySize(n, p float64) int {
+	// calculate our bit array size from the probability p and
+	m := -(n * math.Log(p)) / (math.Pow(math.Log(2), 2))
+	return int(m)
+}
+
+func optimalHashFunk(m, n int) int {
+	floatM := float64(m)
+	floatN := float64(n)
+
+	numFuncs := (floatM / floatN) * math.Log(2)
+	return int(numFuncs)
+}
+
+func (b bloom) Add(item string) error {
+	for i := 0; i < b.numHashes; i++ {
+		// Get the hash value
+		_, err := b.hashFuncs[i].Write([]byte(item))
+		if err != nil {
+			return fmt.Errorf("failed to hash the item, %v", err)
+		}
+
+		index := b.hashFuncs[i].Sum64() % uint64(b.bitArraySize)
+
+		// Insert to bit array
+		_, err = b.bitArray.Put(int(index), 1)
+		if err != nil {
+			return fmt.Errorf("failed to put bitarray item: %v", index)
+		}
+	}
+	return nil
+}
+
+func (b bloom) Check(item string) (bool, error) {
+	for i := 0; i < b.numHashes; i++ {
+		// Get the hash value
+		_, err := b.hashFuncs[i].Write([]byte(item))
+		if err != nil {
+			return false, fmt.Errorf("failed to hash the item, %v", err)
+		}
+
+		index := b.hashFuncs[i].Sum64() % uint64(b.bitArraySize)
+
+		// Check for existence
+		res, err := b.bitArray.Get(int(index))
+		if err != nil {
+			return false, fmt.Errorf("failed to check index, %v", err)
+		}
+		if res == 0 {
+			return false, nil
+		}
+	}
+	return true, nil
 }
--- pkg/bloom/bloom_test.go
+++ pkg/bloom/bloom_test.go
@@ -1,7 +1,10 @@
 package bloom
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,7 +12,7 @@ import (
 func TestNew(t *testing.T) {
 	tables := []struct {
 		capacity      int
-		errorRate     float32
+		errorRate     float64
 		expectedError bool
 	}{
 		{1, 0.1, false},
@@ -18,7 +21,7 @@ func TestNew(t *testing.T) {
 	}
 
 	for _, table := range tables {
-		_, err := New(1, table.capacity, table.errorRate)
+		_, err := New(table.capacity, table.errorRate)
 
 		if table.expectedError {
 			assert.Error(t, err)
@@ -26,4 +29,67 @@ func TestNew(t *testing.T) {
 			assert.NoError(t, err)
 		}
 	}
+}
+
+func TestBitArraySize(t *testing.T) {
+	tables := []struct {
+		n float64
+		p float64
+		m float64
+	}{
+		{20, 0.05, 124},
+	}
+
+	for _, table := range tables {
+		actualM := optimalBitArraySize(table.n, table.p)
+		assert.EqualValues(t, table.m, actualM)
+	}
+}
+
+func TestOptimalHashFunk(t *testing.T) {
+	tables := []struct {
+		m        int
+		n        int
+		numFuncs int
+	}{
+		{124, 20, 4},
+	}
+	for _, table := range tables {
+		actualM := optimalHashFunk(table.m, table.n)
+		assert.EqualValues(t, table.numFuncs, actualM)
+	}
+}
+
+func TestBloom(t *testing.T) {
+	sut, err := New(100, 0.001)
+	assert.NoError(t, err)
+
+	testCycles := 100
+
+	// Start the test
+	startTime := time.Now().UnixNano()
+
+	// Load every other entry
+	for i := 0; i < testCycles; i = i + 2 {
+		iAsString := strconv.Itoa(i) + "meow"
+		currentTime := time.Now().UnixNano()
+		err := sut.Add(iAsString)
+		currentTime2 := time.Now().UnixNano()
+		assert.NoError(t, err)
+		fmt.Printf("iter: %v, add item: \"%v\" took: %v ns\n", i, iAsString, currentTime2-currentTime)
+	}
+
+	for i := 0; i < testCycles; i++ {
+		iAsString := strconv.Itoa(i) + "meow"
+		currentTime := time.Now().UnixNano()
+		ok, err := sut.Check(iAsString)
+		currentTime2 := time.Now().UnixNano()
+		assert.NoError(t, err)
+		fmt.Printf("iter: %v, get item: \"%v\", result: %v took: %v ns\n", i, iAsString, ok, currentTime2-currentTime)
+	}
+
+	// End the test
+	endTime := time.Now().UnixNano()
+
+	fmt.Printf("Test took: %v ns\n", endTime-startTime)
 }
